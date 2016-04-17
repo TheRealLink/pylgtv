9212b37135f78b991529dcd84f68e9bb10b09811
Dennis Karpienski <dennis@karpienski.de>
2016-04-17 21:36:36 +0200

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pylgtv',
-      version='0.0.1',
+      version='0.0.2',
       description='Library to control webOS based LG Tv devices',
       url='https://github.com/TheRealLink/pylgtv',
       author='Dennis Karpienski',
